b942b28483b1d578813b31417b6563f634215f47
t-g-j <t.g.j@sol.dk>
2019-05-16 14:55:30 +0200

    Commiting a new Q-learning player class

---
+++ Thors_edit/ludo/Ludo/main.cpp
@@ -0,0 +1,66 @@
+#include "dialog.h"
+#include <QApplication>
+#include "game.h"
+#include <vector>
+#include "ludo_player.h"
+#include "ludo_player_random.h"
+#include "positions_and_dice.h"
+
+#include "q_ludo_player.h"
+
+Q_DECLARE_METATYPE( positions_and_dice )
+
+int main(int argc, char *argv[]){
+    while(true) {
+        QApplication a(argc, argv);
+        qRegisterMetaType<positions_and_dice>();
+
+        //instanciate the players here
+        ludo_player p1, p2;
+        ludo_player_random p3;
+        q_ludo_player p4;
+
+        game g;
+        g.setGameDelay(1); //if you want to see the game, set a delay
+
+        //set up for each player
+        QObject::connect(&g, SIGNAL(player1_start(positions_and_dice)), &p1, SLOT(start_turn(positions_and_dice)));
+        QObject::connect(&p1, SIGNAL(select_piece(int)), &g, SLOT(movePiece(int)));
+        QObject::connect(&g, SIGNAL(player1_end(std::vector < int > )), &p1,
+                         SLOT(post_game_analysis(std::vector < int > )));
+        QObject::connect(&p1, SIGNAL(turn_complete(bool)), &g, SLOT(turnComplete(bool)));
+
+        QObject::connect(&g, SIGNAL(player2_start(positions_and_dice)), &p2, SLOT(start_turn(positions_and_dice)));
+        QObject::connect(&p2, SIGNAL(select_piece(int)), &g, SLOT(movePiece(int)));
+        QObject::connect(&g, SIGNAL(player2_end(std::vector < int > )), &p2,
+                         SLOT(post_game_analysis(std::vector < int > )));
+        QObject::connect(&p2, SIGNAL(turn_complete(bool)), &g, SLOT(turnComplete(bool)));
+
+        QObject::connect(&g, SIGNAL(player3_start(positions_and_dice)), &p3, SLOT(start_turn(positions_and_dice)));
+        QObject::connect(&p3, SIGNAL(select_piece(int)), &g, SLOT(movePiece(int)));
+        QObject::connect(&g, SIGNAL(player3_end(std::vector < int > )), &p3,
+                         SLOT(post_game_analysis(std::vector < int > )));
+        QObject::connect(&p3, SIGNAL(turn_complete(bool)), &g, SLOT(turnComplete(bool)));
+
+        QObject::connect(&g, SIGNAL(player4_start(positions_and_dice)), &p4, SLOT(start_turn(positions_and_dice)));
+        QObject::connect(&p4, SIGNAL(select_piece(int)), &g, SLOT(movePiece(int)));
+        QObject::connect(&g, SIGNAL(player4_end(std::vector < int > )), &p4,
+                         SLOT(post_game_analysis(std::vector < int > )));
+        QObject::connect(&p4, SIGNAL(turn_complete(bool)), &g, SLOT(turnComplete(bool)));
+
+        //* add a GUI
+        Dialog w;
+        QObject::connect(&g, SIGNAL(update_graphics(std::vector < int > )), &w,
+                         SLOT(update_graphics(std::vector < int > )));
+        QObject::connect(&g, SIGNAL(set_color(int)), &w, SLOT(get_color(int)));
+        QObject::connect(&g, SIGNAL(set_dice_result(int)), &w, SLOT(get_dice_result(int)));
+        QObject::connect(&g, SIGNAL(declare_winner(int)), &w, SLOT(get_winner(int)));
+        w.show();
+        //*/
+
+
+        g.start();
+        return a.exec();
+    }
+
+}
---
+++ Thors_edit/ludo/Ludo/q_ludo_player.h
@@ -0,0 +1,53 @@
+#ifndef Q_LUDO_PLAYER_H
+#define Q_LUDO_PLAYER_H
+#include <QObject>
+#include <iostream>
+#include "positions_and_dice.h"
+
+
+struct states{
+    // Adds a many characteristics as thinkeble for the ludo player.
+    /*
+     * 9 states (Enterboard,move to safe zone, Send enemy home, Block,
+     * Move normally, Move to star, Move to globe,
+     * move in safe zone, finish pieces) 16/5-2019
+     * */
+        float enterBoard    = 0;
+        float moveToSafeZone= 0;
+        float sendEnemyHome = 0;
+        float block         = 0;
+        float moveNormal    = 0;
+        float move2Star     = 0;
+        float move2Globe    = 0;
+        float moveInSafeZone= 0;
+        float finishPiece   = 0;
+
+        std::vector<float> gameStates {enterBoard,moveToSafeZone,sendEnemyHome,block,
+                    moveNormal,move2Star,move2Globe,moveInSafeZone,finishPiece};
+};
+
+
+class q_ludo_player: public QObject {
+    Q_OBJECT
+private:
+    std::vector<int> pos_start_of_turn;
+    std::vector<int> pos_end_of_turn;
+    int dice_roll;
+    int make_decision();
+public:
+    q_ludo_player();
+
+    void printQTable();
+
+    states piece1,piece2,piece3,piece4;
+    // 4 Action (move which piece)
+    std::vector<std::vector<float>> QLtable = {piece1.gameStates,piece2.gameStates,piece3.gameStates,piece4.gameStates};
+signals:
+    void select_piece(int);
+    void turn_complete(bool);
+public slots:
+    void start_turn(positions_and_dice relative);
+    void post_game_analysis(std::vector<int> relative_pos);
+};
+
+#endif // Q_LUDO_PLAYER_H
